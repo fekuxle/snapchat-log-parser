9055c6328c789f5aa30e837c652687b71fb7976d
Felix422 <marius.mf2004@gmail.com>
2021-12-25 02:00:49 +0100

    Change messages to enum and add getters

--- src/types.rs
+++ src/types.rs
@@ -1,12 +1,64 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(untagged)]
+pub enum Message {
+    Sent {     
+        #[serde(rename = "To")]
+        to: String,
+        #[serde(rename = "Media Type")]
+        media_type: MediaType,
+        #[serde(rename = "Created", with = "crate::timestamp")]
+        created_at: DateTime<Utc>,
+        #[serde(rename = "Text")]
+        text: String,
+    },
+    Recieved {
+        #[serde(rename = "From")]
+        from: String,
+        #[serde(rename = "Media Type")]
+        media_type: MediaType,
+        #[serde(rename = "Created", with = "crate::timestamp")]
+        created_at: DateTime<Utc>,
+        #[serde(rename = "Text")]
+        text: String,
+    }
+}
+
+impl Message {
+    pub fn recipient(&self) -> String {
+        match self {
+            Message::Recieved { from, ..} => from.to_owned(),
+            Message::Sent { to, .. } => to.to_owned(),
+        }
+    }
+    pub fn media_type(&self) -> MediaType {
+        match self {
+            Self::Recieved { media_type, .. } => media_type.clone(),
+            Self::Sent { media_type, .. } => media_type.clone(),
+        }
+    }
+    pub fn created_at(&self) -> DateTime<Utc> {
+        match self {
+            Self::Recieved { created_at, .. } => created_at.clone(),
+            Self::Sent { created_at, .. } => created_at.clone(),
+        }
+    }
+    pub fn text(&self) -> String {
+        match self {
+            Self::Recieved { text, .. } => text.clone(),
+            Self::Sent { text, ..} => text.clone()
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 pub struct SnapchatData {
     #[serde(rename = "Received Saved Chat History")]
-    pub saved_recieved: Vec<RecievedSavedChat>,
+    pub saved_recieved: Vec<Message>,
     #[serde(rename = "Sent Saved Chat History")]
-    pub sent_recieved: Vec<SentSavedChat>,
+    pub saved_sent: Vec<Message>,
 }
 
 impl SnapchatData {
@@ -19,7 +71,7 @@ impl SnapchatData {
 }
 
 //{"From": "Felix422", "Media Type": "TEXT", "Created": "2021-01-01 12:00:00 UTC", "Text": "Test Message"}
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 pub struct RecievedSavedChat {
     #[serde(rename = "From")]
     pub from: String,
@@ -32,7 +84,7 @@ pub struct RecievedSavedChat {
 }
 
 //{"To": "Felix422", "Media Type": "TEXT", "Created": "2021-01-01 12:00:00 UTC", "Text": "Test Message"}
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 pub struct SentSavedChat {
     #[serde(rename = "To")]
     pub to: String,
@@ -44,7 +96,7 @@ pub struct SentSavedChat {
     pub text: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Copy, Clone, PartialEq)]
 pub enum MediaType {
     #[serde(rename = "TEXT")]
     Text,
